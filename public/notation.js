7f0283a5a1179cbec3bd6ff3489e774c57b142b2
elosine <justinwenloyang@gmail.com>
2020-06-22 19:25:23 -0400

    uu

--- public/notation.js
+++ public/notation.js
@@ -20,36 +20,45 @@ var camera, scene, renderer, canvas;
 //// Camera Position Settings ///
 var CAM_Z = 130;
 var CAM_Y = -195;
-// var CAM_Z = 0;
-// var CAM_Y = -100;
 var CAM_ROTATION_X = rads(0);
 var RUNWAY_ROTATION_X = -25
 //// Scene Size /////////////////
 var SCENE_W = 250;
 var SCENE_H = 600;
 var RUNWAYLENGTH = 600;
+var RUNWAYSTART = RUNWAYLENGTH / 2;
 var RUNWAYLENGTH_FRAMES = RUNWAYLENGTH / PXPERFRAME;
 var TRACK_DIAMETER = 5;
 var TRACK_Y_OFFSET = 3;
-// </editor-fold> *********************************************** //
-// <editor-fold ******** START UP SEQUENCE ********************** //
-// 01 START TIME SYNC ENGINE ----- >
-var ts = timesync.create({
+var PIECEDURSEC = 49 * 60;
+var PIECEDURFRAMES = 0;
+var ts;
+var fingCursor = [];
+var totalPx = RUNWAYLENGTH;
+// START TIME SYNC ENGINE ----- >
+ts = timesync.create({
   //server: 'https://safe-plateau-48516.herokuapp.com/timesync',
   server: '/timesync',
   interval: 1000
 });
-// 02 MAKE NOTATION OBJECTS ------ >
-
-// 03 START CLOCK SYNC ----------- >
-startClockSync();
-// 04 BEGIN ANIMATION ------------ >
-requestAnimationFrame(animationEngine);
+var startpx = 0;
+var scalePerFrame = 0;
+var followCycScale = 1;
+var flash = true;
+var fingMatl = new THREE.MeshBasicMaterial({
+  color: 0xffff00
+});
+var fingFlashMatl = new THREE.MeshBasicMaterial({
+  color: 0xFF0000
+});
+var countIn;
+var countInCt = 0;
 // </editor-fold> *********************************************** //
+// START PIECE //////////////////////////////////////
+startPiece(0.5);
 // <editor-fold *********** CREATE SCENE ************************ //
-
 // FUNCTION: createScene ------------------------------------------------------------- //
-function create3jsScene(canvasDivId, w, h, length) {
+function create3jsScene(canvasDivId, w, h, length, fingStartPx) {
   // CAMERA ////////////////////////////////////////////////////////////////////
   camera = new THREE.PerspectiveCamera(75, w / h, 1, 3000);
   camera.position.set(0, CAM_Y, CAM_Z);
@@ -62,7 +71,6 @@ function create3jsScene(canvasDivId, w, h, length) {
   sun.position.set(100, 600, 700);
   scene.add(sun);
   var sun2 = new THREE.DirectionalLight(0x40A040, 0.6);
-  // sun2.position.set(-100, 350, 200);
   sun2.position.set(-100, 350, 775);
   scene.add(sun2);
   // RENDERER //////////////////////////////////////////////////////////////////
@@ -105,28 +113,36 @@ function create3jsScene(canvasDivId, w, h, length) {
     var t_fret = new THREE.Mesh(fretGeom, fretMatl);
     t_fret.rotation.z = rads(-90);
     t_fret.position.z = -(length / 2);
-    t_fret.position.y = (RUNWAYLENGTH / 2) - (t_fretGap * i);
+    t_fret.position.y = RUNWAYSTART - (t_fretGap * i);
     conveyor.add(t_fret);
   }
-
-  var fingGeom = new THREE.SphereGeometry(9, 32, 32);
-  var fingMatl = new THREE.MeshBasicMaterial({
-    color: 0xffff00
-  });
+  // FINGER CURSOR ///////////////////////////////////////////////////////
+  // SPHERE ---------------- >
+  var fingGeom = new THREE.SphereGeometry(10, 32, 32);
   var fing = new THREE.Mesh(fingGeom, fingMatl);
   fing.position.z = -(length / 2);
-  fing.position.y = (RUNWAYLENGTH / 2);
+  fing.position.y = fingStartPx;
+  fingCursor.push(fing);
+  // FOLLOW FRET ----------- >
+  var followFretGeom = new THREE.CylinderGeometry(3, 3, t_runwayW, 32);
   var followFretMatl = new THREE.MeshLambertMaterial({
     color: 0xffff00
   });
   var t_ffret = new THREE.Mesh(fretGeom, followFretMatl);
   t_ffret.rotation.z = rads(-90);
   t_ffret.position.z = -(length / 2);
-  t_ffret.position.y = (RUNWAYLENGTH / 2);
-  // MOVE FINGER ///////////////////////////////////////////////////////////////
-  t_ffret.position.y = (RUNWAYLENGTH / 2) - 100;
-  fing.position.y = (RUNWAYLENGTH / 2) - 100;
-  //ADD growing yelow track a little bigger than track
+  t_ffret.position.y = fingStartPx;
+  fingCursor.push(t_ffret);
+  // FOLLOW TRACK ---------- >
+  var ftrgeom = new THREE.CylinderGeometry((TRACK_DIAMETER + 2), (TRACK_DIAMETER + 2), (RUNWAYSTART - fingStartPx), 32);
+  var ftrmatl = new THREE.MeshLambertMaterial({
+    color: 0xffff00
+  });
+  var ftr = new THREE.Mesh(ftrgeom, ftrmatl);
+  ftr.position.z = -(length / 2);
+  ftr.position.y = fingStartPx + ((RUNWAYSTART - fingStartPx) / 2);
+  fingCursor.push(ftr);
+  conveyor.add(ftr);
   conveyor.add(t_ffret);
   conveyor.add(fing);
   // ROTATE GROUP ///////////////////////////////////////////////////////////////
@@ -145,7 +161,7 @@ function mkSVGcanvas(ix, w, h) {
   tsvgCanvas.style.backgroundColor = "black";
   return tsvgCanvas;
 }
-
+// MAKE CANVAS DIV ----------------------------------
 function mkCanvasDiv(ix, w, h, clr) {
   var t_div = document.createElement("div");
   t_div.style.width = w.toString() + "px";
@@ -154,10 +170,6 @@ function mkCanvasDiv(ix, w, h, clr) {
   t_div.id = "canvas" + ix.toString();
   return t_div;
 }
-var cvsDiv = mkCanvasDiv(0, SCENE_W, SCENE_H, '#000000');
-mkpanel(0, cvsDiv, 'center-top', SCENE_W, SCENE_H, 'A String');
-create3jsScene('canvas0', SCENE_W, SCENE_H, RUNWAYLENGTH);
-// create3jsScene();
 // MAKE JSPANEL -------------------------------------
 function mkpanel(ix, content, posString, w, h, title) {
   var tpanel;
@@ -190,7 +202,13 @@ function mkpanel(ix, content, posString, w, h, title) {
 // <editor-fold ********* ANIMATION ENGINE ********************** //
 // UPDATE -------------------------------------------
 function update(aMSPERFRAME, currTimeMS) {
+  mvFinger();
   framect++;
+}
+// DRAW -------------------------------------------
+function draw() {
+  // RENDER ///////////////////////////////////////////////////////////////
+  renderer.render(scene, camera);
 }
 // ANIMATION ENGINE ---------------------------------
 function animationEngine(timestamp) {
@@ -200,26 +218,67 @@ function animationEngine(timestamp) {
   lastFrameTimeMs = t_lt;
   while (delta >= MSPERFRAME) {
     update(MSPERFRAME, t_lt);
+    draw();
     delta -= MSPERFRAME;
   }
-  requestAnimationFrame(animationEngine);
+  if (framect <= PIECEDURFRAMES) {
+    requestAnimationFrame(animationEngine);
+  }
 }
 // </editor-fold> *********************************************** //
 // <editor-fold ******** START UP FUNCTIONS ********************* //
 // START PIECE ----------------------------------------
-function startPiece() {
-  startClockSync();
-  requestAnimationFrame(animationEngine);
+function startPiece(pieceDur) {
+  PIECEDURSEC = pieceDur * 60;
+  PIECEDURFRAMES = PIECEDURSEC * FRAMERATE;
+  // 01 MAKE SCORE ----------------- >
+  var t_leadPx = rrandInt(0, 150);
+  startpx = RUNWAYSTART - t_leadPx;
+  totalPx = RUNWAYLENGTH - t_leadPx;
+  PXPERFRAME = totalPx / PIECEDURFRAMES;
+  // calc scaling for follow track cylinder
+  var t_targetScale = RUNWAYLENGTH / t_leadPx;
+  scalePerFrame = (t_targetScale - 1) / PIECEDURFRAMES;
+  // 02 MAKE NOTATION OBJECTS ------ >
+  var cvsDiv = mkCanvasDiv(0, SCENE_W, SCENE_H, '#000000');
+  mkpanel(0, cvsDiv, 'center-top', SCENE_W, SCENE_H, 'A String');
+  create3jsScene('canvas0', SCENE_W, SCENE_H, RUNWAYLENGTH, startpx);
+  // 03 PAUSE 3 SEC BEFORE BEGINING ANIMATION -- >
+  // Flash CountIn
+  window.setTimeout(function() {
+    var countIn = window.setInterval(function() {
+      if (flash) {
+        if (countInCt < 4) {
+          countInCt++;
+          fingCursor[0].material = fingFlashMatl;
+        }
+        renderer.render(scene, camera);
+        flash = false;
+        // }
+      } else {
+        fingCursor[0].material = fingMatl;
+        renderer.render(scene, camera);
+        flash = true;
+      }
+    }, 500);
+    window.setTimeout(function() {
+      window.clearInterval(countIn);
+      fingCursor[0].material = fingMatl;
+      var t_now = new Date(ts.now());
+      lastFrameTimeMs = t_now.getTime();
+      requestAnimationFrame(animationEngine);
+    }, 4500);
+  }, 5000);
 }
-// CLOCK SYNC -----------------------------------------
-function startClockSync() {
-  var t_now = new Date(ts.now());
-  lastFrameTimeMs = t_now.getTime();
+
+// </editor-fold> *********************************************** //
+// <editor-fold ******** OTHER FUNCTIONS ********************* //
+// MOVE FINGER ----------------------------------------
+function mvFinger() {
+  fingCursor[0].position.y = startpx - (PXPERFRAME * framect);
+  fingCursor[1].position.y = startpx - (PXPERFRAME * framect);
+  followCycScale += scalePerFrame;
+  fingCursor[2].scale.set(1, followCycScale, 1);
+  fingCursor[2].position.y = startpx + (((RUNWAYSTART - startpx) * followCycScale) / 2) - (PXPERFRAME * framect);
 }
 // </editor-fold> *********************************************** //
-
-/* NOTES
-Make String Gliss Animation
-Bring over ThreeJs
-
-*/
